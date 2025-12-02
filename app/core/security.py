51d5e23fac39ef661b5db06e7ce43de0d1e8c2b4
cnv1902tt2 <cnv1902.tiktok2@gmail.com>
2025-12-03 04:37:05 +0700

    a

--- app/core/security.py
+++ app/core/security.py
@@ -4,7 +4,7 @@ import jwt
 from passlib.context import CryptContext
 from .config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__truncate_error=False)
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
